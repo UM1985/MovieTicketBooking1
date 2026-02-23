7b29752017ce0b8e9920185a82258d1e0bcafb2a
UM1985 <56utkarshpatel@gmail.com>
2026-02-23 11:48:56 +0530

    working....

---
+++ server/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log('Mongoose is connected')
+        })
+        await mongoose.connect(`${process.env.MONGO_URI}/quickshow`)
+    }catch (error) {
+        console.log( error.message)
+    }
+}
+
+export default connectDB;
